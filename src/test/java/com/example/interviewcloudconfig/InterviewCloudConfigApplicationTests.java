75ea60b21da6b1f78c0be2218ee58fc1d71ab43d
peyman mahdikhani <payman.mahdikhani@gmail.com>
2022-08-19 12:19:26 +0430

    first commit

---
+++ src/test/java/com/example/interviewcloudconfig/InterviewCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interviewcloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewCloudConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
